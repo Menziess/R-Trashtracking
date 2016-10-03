a76f7acc51c934a5b35de5f783a743b9b4e3217f
Menzies <stefan_schenk@hotmail.com>
2016-10-03 15:29:24 +0200

    Created interactive map with random points

---
+++ R/require.R
@@ -0,0 +1,11 @@
+# Install required packages
+
+list.of.packages <- c("shiny", "XML", "dplyr", "mlxR", "leaflet")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+library(shiny)
+library(XML)
+library(dplyr)
+library(mlxR)
+library(leaflet)
---
+++ R/server.R
@@ -0,0 +1,27 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+source('require.R')
+
+r_colors <- rgb(t(col2rgb(colors()) / 255))
+names(r_colors) <- colors()
+
+shinyServer(function(input, output) {
+
+  points <- eventReactive(input$recalc, {
+    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
+  }, ignoreNULL = FALSE)
+  
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles("Stamen.TonerLite",
+                       options = providerTileOptions(noWrap = TRUE)
+      ) %>%
+      addMarkers(data = points())
+  })
+
+})
