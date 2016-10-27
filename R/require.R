f097a0dcb4c6f78dea9e7026f63d0e228735ce0b
Menzies <stefan_schenk@hotmail.com>
2016-10-27 13:27:27 +0200

    Removed dependency for mac users

--- R/require.R
+++ R/require.R
@@ -2,14 +2,13 @@
 
 options(shiny.trace = T)
 
-list.of.packages <- c("shiny", "XML", "dplyr", "mlxR", "leaflet", "httr", "xml2")
+list.of.packages <- c("shiny", "XML", "dplyr", "leaflet", "httr", "xml2")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
 library(shiny)
 library(XML)
 library(dplyr)
-library(mlxR)
 library(leaflet)
 library(httr)
 library(xml2)
