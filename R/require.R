8a9b5c17db5886179faecc46553c1f9a9ccbf4a7
Menzies <stefan_schenk@hotmail.com>
2016-11-14 17:13:19 +0100

    Testing with cluster and distance matrices

--- R/require.R
+++ R/require.R
@@ -2,7 +2,7 @@
 
 options(shiny.trace = T)
 
-list.of.packages <- c("shiny", "XML", "dplyr", "leaflet", "httr", "xml2", "foreach", "doParallel", "geosphere", "ethodss")
+list.of.packages <- c("shiny", "XML", "dplyr", "leaflet", "httr", "xml2", "foreach", "doParallel", "geosphere", "methods")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
