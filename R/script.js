ea6e65f5b958a90f479bf8791242babd5c5727da
Menzies <stefan_schenk@hotmail.com>
2016-12-24 19:46:16 +0100

    Loading modal on first visit

--- R/script.js
+++ R/script.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
   setTimeout(function() {
     $(".leaflet-right").remove();
-    $('#myModal').modal(); 
   }, 1000);
+  
+  $(window).ready(function(){
+    setTimeout(function() {
+      if (typeof(Storage) !== "undefined") {
+        if (!localStorage.getItem("help")) {
+          $('#myModal').modal('show');
+          localStorage.setItem("help", true);
+        }
+      }
+    }, 2000);
+  });
 });
+
 
 /* Set the transform translateX to 0 to collapse the sidebar */
 function openSidebar() {
